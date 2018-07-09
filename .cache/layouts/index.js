717c7d0159a2f041bddc2ca05b68c418fbe630de
Calum Ryan <hello@calumryan.com>
2018-07-09 22:26:24 +0100

    Update react

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Web/culturebook.github.io/src/layouts/index.js"
+  import data from "/Web/culturebook.github.io/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
